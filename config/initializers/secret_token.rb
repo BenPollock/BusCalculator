5ec8cda02c2ae8271f15aff0d10bfff8c46a4ca8
Ben Pollock <B.O.Pollock@gmail.com>
2013-06-20 19:46:11 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BusCalculator::Application.config.secret_token = 'de358ea416c7682a25b4d0fc385dbb58d711fd051734ef1942c6bf0be1b49b3f6fbd3d2fd351547271b21c4cd2482dbaaeb45cae73ec3e9766ed4ad6eaf5d5df'
