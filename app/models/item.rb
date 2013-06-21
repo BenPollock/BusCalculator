44b45aa19b6742b7334b7cc7b02a88bd6b96cea6
Ben Pollock <B.O.Pollock@gmail.com>
2013-06-21 12:41:47 -0400

    Users specialized in x list

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,11 +4,22 @@ class Item < ActiveRecord::Base
 
 
   def busFactor
-  	@busFactor = self.specialties.count;
+  	@busFactor = self.specialties.count
   end
 
   def specialty?(user)
     @specialties = Specialty.where("item_id = ? AND user_id = ?", self.id, user).count
+  end
+
+  def specializers
+  	@specialties = self.specialties
+  	@users = []
+
+  	@specialties.each do |record|
+  		user = User.find(record.user_id)
+  		@users << user
+  	end
+  	return @users
   end
 
 end
